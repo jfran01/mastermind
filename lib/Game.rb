8de9295723b76a55201959b614ec6989d8a7eafa
Juliette <211356692+jfran01@users.noreply.github.com>
2026-06-11 09:20:50 +0100

    generate code depending on user as setter/breaker

--- lib/Game.rb
+++ lib/Game.rb
@@ -3,7 +3,7 @@ require_relative "Computer"
 
 class Game 
   attr_reader :guesses, :current_guesses
-  
+
   def initialize
     @user = User.new()
     @computer = Computer.new(@user)
@@ -12,12 +12,24 @@ class Game
     @user.get_role
   end
 
+  def get_code 
+    if @user.role == "setter"
+      @code = @user.get_input
+    else @code = @computer.generate_code
+    end
+  end
+
   def get_guess
     if @user.role == "breaker"
       @current_guess = @user.get_input
     else @current_guess = [1, 2, 3, 4]
     end
-    @guesses.push(@current_guess)
+  end
+
+  def check_guess
+    @current_guess.each_with_index do |guess, i|
+      
+    end
   end
 end
 
